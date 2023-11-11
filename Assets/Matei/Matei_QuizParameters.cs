b87a8d9ec70cf24135c7b4a5fe1cfb0dc23799b6
Goidan Matei-Constantin <goidan.mateiconstantin@gmail.com>
2023-11-11 16:56:58 +0200

    Parameters for categories

---
+++ Assets/Matei/Matei_QuizParameters.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum category
+{
+    Linux,
+    DevOps,
+    Networking,
+    Programming,
+    Cloud,
+    Docker,
+    Kubernetes
+}
